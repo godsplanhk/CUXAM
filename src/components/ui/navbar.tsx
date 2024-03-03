6acc599987d8824fcb26a13ce74a69cfc7c96d4f
godsplanhk <godsplanhk@gmail.com>
2024-03-03 14:49:47 +0530

    added navbar

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Turn as Hamburger } from 'hamburger-react'
+import { cn } from '../../lib/utils';
+import { useRecoilState } from 'recoil';
+import { isSidebarOpenState } from '@/state/atoms/sidebar';
+
+interface NavbarProps extends React.HTMLAttributes<HTMLDivElement>{
+    children?: React.ReactNode;
+    className?: string;
+}
+export function Navbar({className, children, ...props}: NavbarProps){
+    const [isSidebarOpen, setIsSidebarOpen] = useRecoilState(isSidebarOpenState); 
+    return <div className={cn("flex justify-between items-center h-16", className)} {...props}>
+              <Hamburger size={16} toggled={isSidebarOpen} toggle={setIsSidebarOpen}></Hamburger>
+        {children}
+        </div>
+    }
