6e4307b14a255bfb2aaac0fa0fbca12fb0c2b1b1
godsplanhk <godsplanhk@gmail.com>
2024-03-04 20:51:28 +0530

    Hamburger improved

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps extends React.HTMLAttributes<HTMLDivElement>{
 export function Navbar({className, children, ...props}: NavbarProps){
     const [isSidebarOpen, setIsSidebarOpen] = useRecoilState(isSidebarOpenState); 
     return <div className={cn("flex justify-between items-center h-16", className)} {...props}>
-              <Hamburger size={16} toggled={isSidebarOpen} toggle={setIsSidebarOpen}></Hamburger>
+              <Hamburger size={26} toggled={isSidebarOpen} toggle={setIsSidebarOpen}></Hamburger>
         {children}
         </div>
     }
