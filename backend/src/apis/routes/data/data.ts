0dbb6476ebe7eee5831eae65373d1eec13c97490
godsplanhk <godsplanhk@gmail.com>
2024-03-25 23:10:06 +0530

    section api

--- backend/src/apis/routes/data/data.ts
+++ backend/src/apis/routes/data/data.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { prisma } from '../../../data/client.js';
+import { getExamAtoms } from "../../../data/queries.js";
 
 const router = express.Router();
 
@@ -11,4 +12,11 @@ router.get('/rooms',async (req,res)=>{
     res.send(await prisma.rooms.findMany());
 })
 
+router.get('/sections',async (req,res)=>{
+    res.send(await prisma.section.findMany());
+})
+router.post('/getExamAtoms',async (req,res)=>{
+    const section = req.body;
+    res.send(await getExamAtoms(section));
+})
 export default router;
