241899e2bc0700254274b8884eff8f36a12b3eb1
godsplanhk <godsplanhk@gmail.com>
2024-03-27 21:20:29 +0530

    type fix

--- src/apis/routes/algo/generate.ts
+++ src/apis/routes/algo/generate.ts
@@ -7,11 +7,11 @@ const router = express.Router();
 router.post('/getSchedule',async (req,res)=>{
     const body = (req.body);
     const batches = body.batches;
-    const batchesId = batches.map(e => e.id);
+    const batchesId = batches.map((e: { id: string; }) => e.id);
     const labs = body.labs;
     const teachers = body.teachers;
     let dates = body.dates;
-    dates = dates.map(e=>new Date(e));
+    dates = dates.map((e: string| Date)=>new Date(e));
     const DatesheetRes = await Generate(batchesId,labs,dates,teachers);
     res.json(DatesheetRes).header({'Access-Control-Allow-Origin': '*',});
 });
