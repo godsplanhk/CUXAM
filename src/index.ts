fcaccf59dcd92014ae2ca95b15e17376e565e0a8
godsplanhk <godsplanhk@gmail.com>
2024-03-29 17:44:06 +0530

    added async error handling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import ControllerRouter from "./apis/controller/controller.js";
 import cors from 'cors';
+import "express-async-errors";
 dotenv.config()
 const PORT = process.env.PORT|| 3000;
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json({limit: '200kb'}));
 app.use('/api/v1',ControllerRouter);
 app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
     console.error(err.stack)
+    console.error(err.name);
+    console.error(err.message);
     res.status(500).send('Something broke!')
     return;
   })
