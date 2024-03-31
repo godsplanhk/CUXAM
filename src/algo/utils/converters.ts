1ebd54113efd6bd1e5c6ae042e610947be89496a
godsplanhk <godsplanhk@gmail.com>
2024-03-31 15:47:24 +0530

    type fix

--- src/algo/utils/converters.ts
+++ src/algo/utils/converters.ts
@@ -5,8 +5,8 @@ import { lSchedule } from "../../types/algoAtoms.js";
 export function convertlScheduleToDBSchedule(e: lSchedule):Schedule {
     return (
         {created: new Date(),
-            internal: e.exam.Teacher,
-            Ccode: e.exam.Ccode,
+            internal: e.exam.teacher.ECode,
+            Ccode: e.exam.course.code,
             sectionId: e.exam.sec.id,
             capacity: e.exam.sec.capacity,
             labNo: e.venue.labNo,
@@ -22,11 +22,11 @@ export interface ScheduleQuery extends Schedule{
     externalR: Teacher
     iTeacher: Teacher
 }
-export async function convertDBScheduleTolSchedule(e: ScheduleQuery):Promise<lSchedule>{
+// export async function convertDBScheduleTolSchedule(e: ScheduleQuery):Promise<lSchedule>{
 
-    return {
-        exam: {Ccode:e.Ccode,Teacher: e.iTeacher.ECode,sec:{id:e.sectionId,capacity:e.capacity}},
-        venue: {labNo:e.labNo,block:e.block,capacity:e.lab.capacity,date: e.date, timeSlot:e.timeSlot},
-        external:e.externalR
-    }
-}
+//     return {
+//         exam: {Ccode:e.Ccode,Teacher: e.iTeacher.ECode,sec:{id:e.sectionId,capacity:e.capacity}},
+//         venue: {labNo:e.labNo,block:e.block,capacity:e.lab.capacity,date: e.date, timeSlot:e.timeSlot},
+//         external:e.externalR
+//     }
+// }
